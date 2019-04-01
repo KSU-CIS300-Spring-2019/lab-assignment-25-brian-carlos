4945a39c473ae4fff176a3adb80a1cce5d13e181
Brian Carlos <briancarlos@ksu.edu>
2019-04-01 11:46:05 -0500

    Done.

---
+++ Ksu.Cis300.PriorityQueueLibrary/LeftistTree.cs
@@ -0,0 +1,75 @@
+﻿/* BinaryTreeNode.cs
+ * Author: Rod Howell */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KansasStateUniversity.TreeViewer2;
+
+namespace Ksu.Cis300.PriorityQueueLibrary
+{
+    /// <summary>
+    /// An immutable generic binary tree node that can draw itself.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements stored in the tree.</typeparam>
+    public partial class LeftistTree<T>
+    {
+        /// <summary>
+        /// Gets the data stored in this node.
+        /// </summary>
+        public T Data { get; }
+
+        /// <summary>
+        /// Gets this node's left child.
+        /// </summary>
+        public LeftistTree<T> LeftChild { get; }
+
+        /// <summary>
+        /// Gets this node's right child.
+        /// </summary>
+        public LeftistTree<T> RightChild { get; }
+
+        /// <summary>
+        /// Stores the null path length
+        /// </summary>
+        private int _nullPathLength = 0;
+
+        /// <summary>
+        /// Constructs a LeftistTree with the given data, left child, and right child.
+        /// </summary>
+        /// <param name="data">The data stored in the node.</param>
+        /// <param name="left">The left child.</param>
+        /// <param name="right">The right child.</param>
+        public LeftistTree(T data, LeftistTree<T> left, LeftistTree<T> right)
+        {
+            Data = data;
+            
+            if (NullPathLength(left) < NullPathLength(right))
+            {
+                RightChild = left;
+                LeftChild = right;
+            }
+            else
+            {
+                RightChild = right;
+                LeftChild = left;
+            }
+            _nullPathLength = NullPathLength(RightChild) + 1;
+        }
+
+        /// <summary>
+        /// Checks if tree is null
+        /// </summary>
+        /// <param name="t">Tree to check</param>
+        /// <returns>0 if null, Null Path Length otherwise</returns>
+        public static int NullPathLength(LeftistTree<T> t)
+        {
+            if (t == null)
+            {
+                return 0;
+            }
+            return t._nullPathLength;
+        }
+    }
+}
